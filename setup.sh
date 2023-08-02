ddec7fbeb5fb7027f7339700bf1c2179710a1841
alphaCentauriCode <alpha.centauri.trades@gmail.com>
2023-08-02 14:50:14 +0200

    zsh config update

--- setup.sh
+++ setup.sh
@@ -2,5 +2,9 @@ mkdir ~/.fonts
 rm -rf ~/.config/alacritty
 rm -rf ~/.config/i3
 rm -rf ~/.config/nvim
+rm -rf ~/.zshrc
+rm -rf ~/.oh-my-zsh
 mv ./.fonts/* ~/.fonts
 mv ./.config/* ~/.config/
+mv ./.zshrc ~/
+mv ./.oh-my-zsh/ ~/
